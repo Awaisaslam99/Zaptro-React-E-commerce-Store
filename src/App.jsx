6dd0a440d8ac479d5f75e55cf5257f997a7e8c4c
Awaisaslam99 <anasawaisaslam333@gmail.com>
2026-06-10 21:00:07 +0500

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Products from "./pages/Products";
 import About from "./pages/About";
 import Cart from "./pages/Cart";
